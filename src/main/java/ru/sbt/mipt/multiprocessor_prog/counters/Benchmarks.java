596f432a846e1b20dc1bbb3fa8b078096a423892
EnjeruGorkaya <tanya.meow13@gmail.com>
2018-12-02 00:29:43 +0300

    mvn1

--- src/main/java/ru/sbt/mipt/multiprocessor_prog/counters/Benchmarks.java
+++ src/main/java/ru/sbt/mipt/multiprocessor_prog/counters/Benchmarks.java
@@ -16,56 +16,39 @@ public class Benchmarks {
     private InternetCounter internetCounter = new InternetCounter();
     private LockCounter lockCounter = new LockCounter();
 
-    int iterations = 1000000;
-
 // Synchronized //
     @Benchmark
     @Threads(1)
     public int syn1() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = synchronizedCounter.getCounter();
-        }
+        int result  = synchronizedCounter.getCounter();
         return result;
-    }
+}
 
     @Benchmark
     @Threads(2)
     public int syn2() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = synchronizedCounter.getCounter();
-        }
+        int result  = synchronizedCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(4)
     public int syn4() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = synchronizedCounter.getCounter();
-        }
+        int result  = synchronizedCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(8)
     public int syn8() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = synchronizedCounter.getCounter();
-        }
+        int result  = synchronizedCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(16)
     public int syn16() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = synchronizedCounter.getCounter();
-        }
+        int result  = synchronizedCounter.getCounter();
         return result;
     }
 
@@ -73,50 +56,35 @@ public class Benchmarks {
     @Benchmark
     @Threads(1)
     public int concurrent1() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = concurrentCounter.getCounter();
-        }
+        int result = concurrentCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(2)
     public int concurrent2() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = concurrentCounter.getCounter();
-        }
+        int result = concurrentCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(4)
     public int concurrent4() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = concurrentCounter.getCounter();
-        }
+        int result = concurrentCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(8)
     public int concurrent8() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = concurrentCounter.getCounter();
-        }
+        int result = concurrentCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(16)
     public int concurrent16() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = concurrentCounter.getCounter();
-        }
+        int result = concurrentCounter.getCounter();
         return result;
     }
 
@@ -124,50 +92,35 @@ public class Benchmarks {
     @Benchmark
     @Threads(1)
     public int internet1() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = internetCounter.getCounter();
-        }
+        int result  = internetCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(2)
     public int internet2() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = internetCounter.getCounter();
-        }
+        int result  = internetCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(4)
     public int internet4() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = internetCounter.getCounter();
-        }
+        int result  = internetCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(8)
     public int internet8() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = internetCounter.getCounter();
-        }
+        int result  = internetCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(16)
     public int internet16() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = internetCounter.getCounter();
-        }
+        int result  = internetCounter.getCounter();
         return result;
     }
 
@@ -175,50 +128,35 @@ public class Benchmarks {
     @Benchmark
     @Threads(1)
     public int locked1() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = lockCounter.getCounter();
-        }
+        int result = lockCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(2)
     public int locked2() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = lockCounter.getCounter();
-        }
+        int result = lockCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(4)
     public int locked4() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = lockCounter.getCounter();
-        }
+        int result = lockCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(8)
     public int locked8() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = lockCounter.getCounter();
-        }
+        int result = lockCounter.getCounter();
         return result;
     }
 
     @Benchmark
     @Threads(16)
     public int locked16() {
-        int result = 0;
-        for (int i = 0; i < iterations; i++) {
-            result = lockCounter.getCounter();
-        }
+        int result = lockCounter.getCounter();
         return result;
     }
 }
